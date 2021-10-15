983e4028e6ac373788581d83f6393a08a37d0853
cado10 <candresdelgado@gmail.com>
2021-10-14 20:28:29 -0500

    Interfaz Productos React

---
+++ ProyectoReactCLiente/NatuMarket/src/Datos/DatosCuerpoProductos.js
@@ -0,0 +1,49 @@
+const DatosCuerpoProductos = [
+	{
+		categoria: 'Granos',
+		codigo: '12345',
+		descripcion: 'Frijoles',
+		imagen: 'Ricos frijoles negros',
+		stock: '40',
+        precio_compra: '2000',
+        precio_venta: '3000',
+        unidad_medida: 'libra',
+        estado: true,
+	},
+	{
+		categoria: 'Bebidas',
+		codigo: '12334',
+		descripcion: 'Leche Almendras',
+		imagen: 'Con calcio y vitamina D',
+		stock: '50',
+        precio_compra: '12000',
+        precio_venta: '15000',
+        unidad_medida: 'litro',
+        estado: true,
+	},
+	{
+		categoria: 'Semillas',
+		codigo: '1232454',
+		descripcion: 'Leche Almendras',
+		imagen: 'Con calcio y vitamina D',
+		stock: '50',
+        precio_compra: '12000',
+        precio_venta: '15000',
+        unidad_medida: 'litro',
+        estado: true,
+	},
+	{
+		categoria: 'Snacks',
+		codigo: '123435',
+		descripcion: 'Leche Almendras',
+		imagen: 'Con calcio y vitamina D',
+		stock: '50',
+        precio_compra: '12000',
+        precio_venta: '15000',
+        unidad_medida: 'litro',
+        estado: true,
+	},
+];
+
+export default DatosCuerpoProductos;
+

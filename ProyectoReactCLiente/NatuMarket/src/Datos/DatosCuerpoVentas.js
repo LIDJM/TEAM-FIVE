12201ea7e05d9143f981a803151784d9019687b8
Jose Valencia <jivalenciadiaz@gmail.com>
2021-10-18 19:27:21 -0500

    updating backend, and frontend in react for ventas module

---
+++ ProyectoReactCLiente/NatuMarket/src/Datos/DatosCuerpoVentas.js
@@ -0,0 +1,40 @@
+const DatosCuerpoVentas = [
+	{
+		vendedor: 'Carlos Madrigal',
+		cliente: '12345',
+		producto: 'Frijoles',
+		cantidad: 25,
+        precio_unitario: 3000,
+		precio_venta_total: '',
+        fecha: '18/10/2021'
+	},
+    {
+		vendedor: 'Carlos Madrigal',
+		cliente: '12345',
+		producto: 'Frijoles',
+		cantidad: 25,
+        precio_unitario: 3000,
+		precio_venta_total: '',
+        fecha: '18/10/2021'
+	},
+    {
+		vendedor: 'Carlos Madrigal',
+		cliente: '12345',
+		producto: 'Frijoles',
+		cantidad: 25,
+        precio_unitario: 3000,
+		precio_venta_total: '',
+        fecha: '18/10/2021'
+	},
+    {
+		vendedor: 'Carlos Madrigal',
+		cliente: '12345',
+		producto: 'Frijoles',
+		cantidad: 25,
+        precio_unitario: 3000,
+		precio_venta_total: '',
+        fecha: '18/10/2021'
+	},
+];
+
+export default DatosCuerpoVentas;
--- ProyectoReactCLiente/NatuMarket/src/components/pages/ventas.js
+++ ProyectoReactCLiente/NatuMarket/src/components/pages/ventas.js
@@ -1,11 +1,82 @@
 import React from 'react';
-// import Tabla from '../components/Tabla';
+import Tabla from '../components/Tabla';
+import DatosCabeceraVentas from '../Datos/DatosCabeceraVentas';
+import DatosCuerpoVentas from '../Datos/DatosCuerpoVentas';
 
 const ventas = () => {
+
+	const setVentasTotal = (valor_unitario, cantidad) => {
+		const vt = valor_unitario * cantidad;
+		return vt;
+	}
+
 	return (
-		<div className='ventas'>
-			<h1>Ventas</h1>
-		</div>
+		<>
+			<div className='page_content'>
+				<table id='agregarBuscar'>
+					<div class='row'>
+						<tr>
+							<th>
+								<div class='col-sm-7'>
+									<a href='/registroProductos' class='btn btn-primary'>
+										<i class='material-icons'>&#xE147;</i>
+										<span>Ingresar Producto</span>
+									</a>
+								</div>
+							</th>
+							<th>
+								<div class='input-group'>
+									<div class='form-outline'>
+										<input
+											type='search'
+											id='form1'
+											class='form-control'
+											placeholder='Buscar'
+										/>
+									</div>
+									<button type='button' class='btn btn-primary'>
+										<i class='fa fa-search'></i>
+									</button>
+								</div>
+							</th>
+						</tr>
+					</div>
+				</table>
+				<Tabla 
+					Datos = {
+						<table class='table table-striped table-hover'>
+							<thead>
+								<tr>
+									{DatosCabeceraVentas.map((item, index) => {
+										return <th key='{index}'>{item.nombre}</th>;
+									})}
+								</tr>
+							</thead>
+							<tbody>
+								{DatosCuerpoVentas.map((item, index) => {
+										return (
+											<tr key = {index}>
+												<th> {item.vendedor} </th>
+												<th> {item.cliente} </th>
+												<th> {item.producto} </th>
+												<th> {item.cantidad} </th>
+												<th> {item.precio_unitario} </th>
+												<th>
+													{setVentasTotal(item.precio_unitario, item.cantidad)}
+												</th>
+												<th> {item.fecha} </th>
+											</tr>
+										)
+								})}
+							</tbody>
+
+						</table>
+					}
+				>
+					
+				</Tabla>
+			</div>
+		</>
 	);
 };
 

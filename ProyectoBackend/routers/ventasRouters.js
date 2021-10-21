e3ca62c422789aad53265fb4d53d1e065e6a3784
Jose Valencia <jivalenciadiaz@gmail.com>
2021-10-21 10:12:53 -0500

    updating 404 messages and filtering by several fields

--- ProyectoBackend/routers/ventasRouters.js
+++ ProyectoBackend/routers/ventasRouters.js
@@ -2,7 +2,13 @@ const {Router} = require('express');
 
 const router = Router();
 
-const {getVentas, setVentas, actualizarVenta, eliminarVenta,} = require('../controllers/ventasController');
+const {getVentas, 
+    setVentas, 
+    actualizarVenta, 
+    eliminarVenta, 
+    getVentasByProducto,
+    getVentasByCliente,
+    getVentasByID } = require('../controllers/ventasController');
 
 router.get('/listar', getVentas);
 
@@ -11,5 +17,11 @@ router.post('/registrar', setVentas);
 router.put('/actualizar/:id', actualizarVenta);
 
 router.delete('/eliminar/:id', eliminarVenta);
+
+router.get('/filtroVentaProducto/:producto_id', getVentasByProducto);
+
+router.get('/filtroVentaCliente/:cliente_id', getVentasByCliente);
+
+router.get('/filtroVentaId/:id', getVentasByID);
 
 module.exports = router;

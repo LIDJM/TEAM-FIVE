47cde3a650ca1e3f8d398b8be7fa55b4273489e9
Jose Valencia <jivalenciadiaz@gmail.com>
2021-10-22 18:47:28 -0500

    actualizado modulo ventas en react, llamando el backend conectado

--- ProyectoReactCLiente/NatuMarket/src/routes/ContentRouter.js
+++ ProyectoReactCLiente/NatuMarket/src/routes/ContentRouter.js
@@ -3,8 +3,8 @@ import SideBar from '../components/Menu/SideBar';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import home from '../components/pages/home';
 import productos from '../components/pages/productos';
-import usuarios from '../components/pages/usuarios';
-import ventas from '../components/pages/ventas';
+import Usuarios from '../components/pages/Usuarios';
+import Ventas from '../components/pages/Ventas';
 import registroUsuarios from '../components/pages/registroUsuarios';
 import registroProductos from '../components/pages/registroProductos';
 import registroVentas from '../components/pages/registroVentas';
@@ -16,8 +16,8 @@ const ContentRouter = () => {
 			<Switch>
 				<Route path='/' exact component={home} />
 				<Route path='/productos' exact component={productos} />
-				<Route path='/usuarios' exact component={usuarios} />
-				<Route path='/ventas' exact component={ventas} />
+				<Route path='/usuarios' exact component={Usuarios} />
+				<Route path='/ventas' exact component={Ventas} />
 				<Route
 					path='/registroUsuarios'
 					exact
